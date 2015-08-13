0cd45e473965024603a3a49208d081eb45f4c4c7
Jenkins Autobot <releaseengineering@magnet.com>
2015-08-13 05:49:59 +0000

    Changing version from 1.4.4 to 1.4.5

--- tools/mmx-sanity-app/src/main/scripts/run-sanity.sh
+++ tools/mmx-sanity-app/src/main/scripts/run-sanity.sh
@@ -24,7 +24,7 @@
 #
 
 for dir in . ../../../target; do
-  LIBS=${dir}/mmx-sanity-app-1.4.4-shaded.jar
+  LIBS=${dir}/mmx-sanity-app-1.4.5-shaded.jar
   if [ -f ${LIBS} ]; then
     break;
   fi
--- tools/release-build/mmx-rel-build.sh
+++ tools/release-build/mmx-rel-build.sh
@@ -9,7 +9,7 @@
 #
 # Usage: mmx-rel-build old-version new-version release-branch-name
 #
-# For example: mmx-rel-build 1.4.4 1.0.2 release-1.0.2-RC8
+# For example: mmx-rel-build 1.4.5 1.0.2 release-1.0.2-RC8
 #
 
 mmx-repo() {
