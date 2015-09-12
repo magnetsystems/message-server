8e5a9fbc296a2675b32e1b4f67856418bce29994
Jenkins Autobot <releaseengineering@magnet.com>
2015-09-12 04:24:32 +0000

    Changing version from 1.7.2 to 1.7.3

--- tools/mac-installer/build_pkg.sh
+++ tools/mac-installer/build_pkg.sh
@@ -21,7 +21,7 @@ if [ -v $DEBUG ]; then
     set -x
 fi
 
-export MMX_UNZIP_DIR='mmx-standalone-dist-1.7.2'
+export MMX_UNZIP_DIR='mmx-standalone-dist-1.7.3'
 export MMX_ZIPFILE="${MMX_UNZIP_DIR}.zip"
 
 if [ "$(ls -A payload)" ]; then
--- tools/mmx-sanity-app/src/main/scripts/run-test.sh
+++ tools/mmx-sanity-app/src/main/scripts/run-test.sh
@@ -8,7 +8,7 @@ if [ $# -ne 1 ]; then
 fi
 
 for dir in . ../../../target; do
-  LIBS=${dir}/mmx-sanity-app-1.7.2-shaded.jar
+  LIBS=${dir}/mmx-sanity-app-1.7.3-shaded.jar
   if [ -f ${LIBS} ]; then
     break;
   fi
