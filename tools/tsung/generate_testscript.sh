a8f66d9ded76c34170dab9183514d031bfa17ae6
Jenkins Autobot <releaseengineering@magnet.com>
2015-09-14 19:27:14 +0000

    Changing version from 1.8.2 to 1.8.3

--- tools/tsung/generate_testscript.sh
+++ tools/tsung/generate_testscript.sh
@@ -69,7 +69,7 @@ fi
 
 # now generate the load test script
 
-GEN_SCRIPT_JAR="tooljar/mmx-tools-tsung-1.8.2-shaded.jar"
+GEN_SCRIPT_JAR="tooljar/mmx-tools-tsung-1.8.3-shaded.jar"
 if [ ! -e ${GEN_SCRIPT_JAR} ]; then
     buildCommand="mvn clean install"
     eval "$buildCommand"
