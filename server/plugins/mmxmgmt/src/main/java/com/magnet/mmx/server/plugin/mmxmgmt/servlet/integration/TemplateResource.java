5283c6dd2b5de1277165bb2a937111cb101b9f46
Vincent Lau <vincent.lau@magnet.com>
2016-04-18 13:22:23 -0700

    Remove unused import and add copyright statement.

--- server/plugins/mmxmgmt/src/main/java/com/magnet/mmx/server/plugin/mmxmgmt/servlet/integration/TemplateResource.java
+++ server/plugins/mmxmgmt/src/main/java/com/magnet/mmx/server/plugin/mmxmgmt/servlet/integration/TemplateResource.java
@@ -1,13 +1,21 @@
+/*   Copyright (c) 2016 Magnet Systems, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.magnet.mmx.server.plugin.mmxmgmt.servlet.integration;
 
 import com.magnet.mmx.protocol.TemplateDataModel;
-import com.magnet.mmx.server.common.data.AppEntity;
 import com.magnet.mmx.server.plugin.mmxmgmt.MMXException;
-import com.magnet.mmx.server.plugin.mmxmgmt.db.AppDAO;
-import com.magnet.mmx.server.plugin.mmxmgmt.db.AppDAOImpl;
-import com.magnet.mmx.server.plugin.mmxmgmt.db.OpenFireDBConnectionProvider;
-import com.magnet.mmx.server.plugin.mmxmgmt.pubsub.PubSubWakeupProvider.FmMmxTemplateLoader;
-import com.magnet.mmx.server.plugin.mmxmgmt.pubsub.PubSubWakeupProvider.FmPushConfig;
 import com.magnet.mmx.server.plugin.mmxmgmt.push.config.MMXPushConfigService;
 import com.magnet.mmx.server.plugin.mmxmgmt.push.config.model.MMXTemplate;
 import com.magnet.mmx.server.plugin.mmxmgmt.push.config.model.MMXTemplateType;
@@ -28,7 +36,6 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -40,7 +47,6 @@ import java.util.List;
 public class TemplateResource {
 
     @POST
-//    @Path("")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createTemplate(TemplateDataModel.TemplateRequest request) {
